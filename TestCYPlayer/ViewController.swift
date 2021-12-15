b23e6b279c9138ec9a570a17fc9de700fed092be
yellowei <me@yellowei.com>
2021-12-15 16:13:45 +0800

    Initial Commit

---
+++ TestCYPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCYPlayer
+//
+//  Created by yellowei on 2021/12/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
